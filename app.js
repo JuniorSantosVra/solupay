2102f4f8da34a8ad90642e35429f6b9836f52e21
Hits FM <davidjunior85@gmail.com>
2018-09-23 12:21:58 -0300

    Mundança de porta

--- app.js
+++ app.js
@@ -2,9 +2,11 @@
    , io = require('socket.io').listen(app)
    , fs = require('fs')
  ;
- app.listen(3000, function() {
-   console.log("Servidor rodando!");
- });
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
  function index(req, res){
    fs.readFile(__dirname + '/index.html', function(err, data){
  res.writeHead(200);
